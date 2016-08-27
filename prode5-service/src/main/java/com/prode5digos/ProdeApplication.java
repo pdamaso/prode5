3434e27311335c0690623d209f401f2efbd84902
pdamaso <pablo.damaso@gmail.com>
2016-08-27 01:32:33 -0300

    Initial setting for gradle and modules

---
+++ prode5-service/src/main/java/com/prode5digos/ProdeApplication.java
@@ -0,0 +1,12 @@
+package com.prode5digos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdeApplication.class, args);
+	}
+}
